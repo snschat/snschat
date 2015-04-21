2d23659349a0a7539100845252d44a0ec50a5bc4
rock <covernal@hotmail.com>
2015-04-21 21:33:37 +0800

    initial repository

---
+++ ShopnSocial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopnSocial
+//
+//  Created by rock on 4/21/15.
+//  Copyright (c) 2015 rock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShopnSocial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShopnSocial
+//
+//  Created by rock on 4/21/15.
+//  Copyright (c) 2015 rock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
